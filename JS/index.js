20293c7fdd9a4534d561f93e506eba3f4cf4ee97
Buddynice34 <willemapril33@gmail.com>
2024-01-16 16:38:01 +0200

    updated the navbar
    - added ul and li to the anchor elements
    - updated styling to the nav list items
    - added more functionlity for the nav

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,12 @@
 const menu = document.getElementById('menu');
 const nav = document.querySelector('nav');
-
+const li = document.querySelectorAll('ul li');
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+})
+
+li.forEach(item => {
+    item.addEventListener("click", () =>{
+        nav.classList.remove("active");
+    })
 })
