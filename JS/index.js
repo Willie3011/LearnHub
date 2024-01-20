4cb5f4d1f08cd7d6937b34c2571132cbec46cb95
Buddynice34 <willemapril33@gmail.com>
2024-01-20 09:34:59 +0200

    Updated the burger menu button
    - Used boxicon element instead of css styling for burger menu

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,8 @@ const nav = document.querySelector('nav');
 const li = document.querySelectorAll('ul li');
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+    const menuButton = document.querySelector('button i');
+    menuButton.classList.toggle("bx-x")
 })
 
 li.forEach(item => {
